219fcf9b7128cae1cef721241ca0d23e734648f1
Cheryl Frankenfield <cherylfrankenfield@gmail.com>
2017-10-18 21:17:33 -0700

    practice input forms

---
+++ js/scripts.js
@@ -0,0 +1,8 @@
+$(document).ready(function(event) {
+  var beverage = $("#beverage").val();
+
+  var flavor = $("input:radio[name=flavor]:checked").val();
+  var dob = $("#born").val();
+  var favoriteColor = $("#color").val();
+  event.preventDefault();
+});
